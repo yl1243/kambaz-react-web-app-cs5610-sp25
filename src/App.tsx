b65a681594a5a630791db110e8a88a90d503f8f3
yl1243 <gloria19984089@gmail.com>
2025-01-17 17:26:43 -0800

    demo again

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more, edited for Assignment1 demooo
+        Click on the Vite and React logos to learn more, edited for Assignment1 demooo000
       </p>
     </>
   )
